da770a8239152da16b972f707b56b2e191c613a5
benjimon <98272636+maybleMyers@users.noreply.github.com>
2025-05-27 09:17:22 -0700

    Update networks.py

--- extensions-builtin/sd_forge_lora/networks.py
+++ extensions-builtin/sd_forge_lora/networks.py
@@ -22,9 +22,9 @@ def load_lora_for_models(model, clip, lora, strength_model, strength_clip, filen
     lora_unet, lora_unmatch = load_lora(lora_unmatch, unet_keys)
     lora_clip, lora_unmatch = load_lora(lora_unmatch, clip_keys)
 
-    if len(lora_unmatch) > 12:
-        print(f'[LORA] LoRA version mismatch for {model_flag}: {filename}')
-        return model, clip
+    #if len(lora_unmatch) > 12:
+        #print(f'[LORA] LoRA version mismatch for {model_flag}: {filename}')
+        #return model, clip
 
     if len(lora_unmatch) > 0:
         print(f'[LORA] Loading {filename} for {model_flag} with unmatched keys {list(lora_unmatch.keys())}')
